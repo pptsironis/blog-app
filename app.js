9597e8d15181749cf847013ef87904383c302e17
P. Tsironis <p.p.tsironis@gmail.com>
2022-11-18 13:10:27 +0200

    challenge 12

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ app.use(express.static("public"));
 const posts = [];
 
 app.get("/", function (req, res){
-  res.render("home", {startingContent: homeStartingContent});
-  console.log(posts);
+  res.render("home", {startingContent: homeStartingContent, posts: posts});
 });
 
 app.get("/about", function (req, res){
