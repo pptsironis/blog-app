2a1433538509ebb06cb2fc52519bb3447db286de
P. Tsironis <p.p.tsironis@gmail.com>
2022-11-18 13:36:03 +0200

    challenge 13

--- app.js
+++ app.js
@@ -17,30 +17,30 @@ app.use(express.static("public"));
 
 const posts = [];
 
-app.get("/", function (req, res){
-  res.render("home", {startingContent: homeStartingContent, posts: posts});
+app.get("/", function (req, res) {
+    res.render("home", {startingContent: homeStartingContent, posts: posts});
 });
 
-app.get("/about", function (req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/about", function (req, res) {
+    res.render("about", {aboutContent: aboutContent});
 });
 
-app.get("/contact", function (req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/contact", function (req, res) {
+    res.render("contact", {contactContent: contactContent});
 });
 
-app.get("/compose", function (req, res){
-  res.render("compose");
+app.get("/compose", function (req, res) {
+    res.render("compose");
 });
-app.post("/compose", function (req,res){
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postContent
-  };
-  posts.push(post)
-  res.redirect("/");
+app.post("/compose", function (req, res) {
+    const post = {
+        title: req.body.postTitle,
+        content: req.body.postContent
+    };
+    posts.push(post)
+    res.redirect("/");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
