bb6e6c7bd86a9dd321511fc960491c0d3345d1d6
P. Tsironis <p.p.tsironis@gmail.com>
2022-11-18 12:53:45 +0200

    challenge 11

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const posts = [];
 
 app.get("/", function (req, res){
   res.render("home", {startingContent: homeStartingContent});
+  console.log(posts);
 });
 
 app.get("/about", function (req, res){
@@ -36,6 +38,8 @@ app.post("/compose", function (req,res){
     title: req.body.postTitle,
     content: req.body.postContent
   };
+  posts.push(post)
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
